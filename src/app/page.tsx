52f1d1a7207336d67a04773996970b8ac7f30a67
behzad aslani <behzadaslani80@gmail.com>
2025-09-08 21:56:36 +0330

    fixed job position section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -30,7 +30,7 @@ export default async function page() {
       <TimeLine />
       <OurTeam />
       <GetInTouch />
-      {/* <JobAccordion /> */}
+      <JobAccordion />
       {/* <Footer /> */}
     </div>
   )
--- src/components/jobAccorion/index.tsx
+++ src/components/jobAccorion/index.tsx
@@ -6,30 +6,34 @@ import JobPosition from './jobPositions'
 export default function JobAccordion() {
   return (
     <section className='relative overflow-hidden'>
-      <h1 className='mb-5 text-center font-goldman text-heading-S-2 text-blue-300 md:text-heading-L-2 lg:text-heading-XL-2'>
+      <h1 className='mb-5 text-center text-lg md:text-xl lg:text-2xl text-blue-300'>
         Job position
       </h1>
-      <div className=' relative z-10 mx-auto mb-10 flex max-w-screen-lg flex-col gap-4 px-[24px] sm:px-[80px] md:px-[140px] lg:px-[240px]'>
+      <div className=' relative z-10 mx-auto mb-10 flex max-w-screen-lg flex-col gap-4 px-[24px] '>
         <JobPosition title='Frontend Developer'>
           <p className=' pb-5 '>we need a junior Developer in our team .</p>
           <h6 className='mb-2 '>Responsibilities</h6>
           <ul className='space-y-1 pl-8'>
             <li className='flex gap-2'>
-              <span>•</span>Design, develop, and maintain innovative features based on business needs
+              <span>•</span>Design, develop, and maintain innovative features
+              based on business needs
             </li>
             <li className='flex gap-2'>
-              <span>•</span>Bring our design system to life by implementing high-fidelity UI using a modern frontend
-              framework
+              <span>•</span>Bring our design system to life by implementing
+              high-fidelity UI using a modern frontend framework
             </li>
             <li className='flex gap-2'>
-              <span>•</span>Ensure high-quality deliverables through CI/CD pipelines, tests, and documentation
+              <span>•</span>Ensure high-quality deliverables through CI/CD
+              pipelines, tests, and documentation
             </li>
             <li className='flex gap-2'>
               <span>•</span>
-              Collaborate with Design, Product Management and Customer Success teams to continuously improve the product
+              Collaborate with Design, Product Management and Customer Success
+              teams to continuously improve the product
             </li>
             <li className='flex gap-2'>
-              <span>•</span>Support the team by actively participating in discussions and reviewing others’ code
+              <span>•</span>Support the team by actively participating in
+              discussions and reviewing others’ code
             </li>
             <li className='flex gap-2'>
               <span>•</span>Improve team synergy and mentor junior engineers
@@ -41,21 +45,25 @@ export default function JobAccordion() {
           <h6 className='mb-2 '>Responsibilities</h6>
           <ul className='space-y-1 pl-8'>
             <li className='flex gap-2'>
-              <span>•</span>Design, develop, and maintain innovative features based on business needs
+              <span>•</span>Design, develop, and maintain innovative features
+              based on business needs
             </li>
             <li className='flex gap-2'>
-              <span>•</span>Bring our design system to life by implementing high-fidelity UI using a modern frontend
-              framework
+              <span>•</span>Bring our design system to life by implementing
+              high-fidelity UI using a modern frontend framework
             </li>
             <li className='flex gap-2'>
-              <span>•</span>Ensure high-quality deliverables through CI/CD pipelines, tests, and documentation
+              <span>•</span>Ensure high-quality deliverables through CI/CD
+              pipelines, tests, and documentation
             </li>
             <li className='flex gap-2'>
               <span>•</span>
-              Collaborate with Design, Product Management and Customer Success teams to continuously improve the product
+              Collaborate with Design, Product Management and Customer Success
+              teams to continuously improve the product
             </li>
             <li className='flex gap-2'>
-              <span>•</span>Support the team by actively participating in discussions and reviewing others’ code
+              <span>•</span>Support the team by actively participating in
+              discussions and reviewing others’ code
             </li>
             <li className='flex gap-2'>
               <span>•</span>Improve team synergy and mentor junior engineers
@@ -67,21 +75,25 @@ export default function JobAccordion() {
           <h6 className='mb-2 '>Responsibilities</h6>
           <ul className='space-y-1 pl-8'>
             <li className='flex gap-2'>
-              <span>•</span>Design, develop, and maintain innovative features based on business needs
+              <span>•</span>Design, develop, and maintain innovative features
+              based on business needs
             </li>
             <li className='flex gap-2'>
-              <span>•</span>Bring our design system to life by implementing high-fidelity UI using a modern frontend
-              framework
+              <span>•</span>Bring our design system to life by implementing
+              high-fidelity UI using a modern frontend framework
             </li>
             <li className='flex gap-2'>
-              <span>•</span>Ensure high-quality deliverables through CI/CD pipelines, tests, and documentation
+              <span>•</span>Ensure high-quality deliverables through CI/CD
+              pipelines, tests, and documentation
             </li>
             <li className='flex gap-2'>
               <span>•</span>
-              Collaborate with Design, Product Management and Customer Success teams to continuously improve the product
+              Collaborate with Design, Product Management and Customer Success
+              teams to continuously improve the product
             </li>
             <li className='flex gap-2'>
-              <span>•</span>Support the team by actively participating in discussions and reviewing others’ code
+              <span>•</span>Support the team by actively participating in
+              discussions and reviewing others’ code
             </li>
             <li className='flex gap-2'>
               <span>•</span>Improve team synergy and mentor junior engineers
