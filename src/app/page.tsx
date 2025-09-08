909a5bb5e6e5aecd06559192e251e5f07a1886d2
behzad aslani <behzadaslani80@gmail.com>
2025-09-08 21:52:27 +0330

    fixed get in touch section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -29,7 +29,7 @@ export default async function page() {
       <OtherBrands />
       <TimeLine />
       <OurTeam />
-      {/* <GetInTouch /> */}
+      <GetInTouch />
       {/* <JobAccordion /> */}
       {/* <Footer /> */}
     </div>
